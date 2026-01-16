6065a3bbc377da4f20f13f13ce2fc49020946687
kuldeepsaini65 <coolboyk.deepsaini@gmail.com>
2026-01-17 00:43:08 +0530

    db pass in .env

--- ECOMM/settings.py
+++ ECOMM/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "ecomm_assignment",
         "USER": "root",
-        "PASSWORD": "",
+        "PASSWORD": env('DATABASE_PASSWORD'),
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
